055b76bba900404bff8669d7ea82561482f175de
zahrasabila <zahra.sabila22@mhs-uinjkt.ac.id>
2024-06-24 12:21:43 +0700

    bismilla

--- server/index.js
+++ server/index.js
@@ -7,21 +7,27 @@ const upload = require("express-fileupload");
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-// Configure CORS to allow multiple origins
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://fpci-blog-app.vercel.app",
-];
-
 const app = express();
-app.use(
-  cors({
-    origin: "https://fpci-blog-app-interface.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-  })
-);
-app.use(express.json({ extended: true }));
+
+// Configure CORS to allow multiple origins
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Check if the origin is allowed
+    const allowedOrigins = [
+      "http://localhost:3000",
+      "https://fpci-blog-app-interface.vercel.app",
+    ];
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(upload());
 app.use("/uploads", express.static(__dirname + "/uploads"));
